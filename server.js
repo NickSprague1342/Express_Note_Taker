f008b6cf31dba393e5a2857b8806365f4eb54bb1
Nick Sprague <n.sprague18@gmail.com>
2024-05-07 13:18:13 -0400

    started server.js, got server running on port 3001

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 let express = require('express');
 let path = require('path');
 let fs = require('fs');
+let util = require('util');
+let { v4: uuid$ } = require('uuid');
+
 let PORT = process.env.PORT || 3001
+
+let readIt = util.promisify(fs.readIt);
+let writeIt = util.promisify(fs.writeIt);
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(_dirname, 'Develop/public/index.html'))
+});
+
+app.get('/notes', (req, resp) => {
+    res.sendFile(path.join(_dirname, 'Develop/public/notes.html'))
+});
