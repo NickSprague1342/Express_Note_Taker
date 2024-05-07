9703dc4dd5631210973dbc7e83bacd92ef8da638
Nick Sprague <n.sprague18@gmail.com>
2024-05-07 15:03:07 -0400

    finished server.js and added comments

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+//=====================================
+// below are all the requires, the port 
+// location, and the read note and write 
+// note variables
+//=====================================
 let express = require('express');
 let path = require('path');
 let fs = require('fs');
@@ -13,6 +18,9 @@ const app = express();
 
 app.use(express.json());
 
+//===================================
+// get requests and paths for notes files
+//===================================
 app.get('/', (req, res) => {
     res.sendFile(path.join(_dirname, 'Develop/public/index.html'))
 });
@@ -21,11 +29,16 @@ app.get('/notes', (req, resp) => {
     res.sendFile(path.join(_dirname, 'Develop/public/notes.html'))
 });
 
+//=================================
+// base function for notes, this is 
+// used to for GET, POST, and DELETE
+// requests for notes
+//=================================
 function findNotes() {
     return readIt('db/db.json').then(newNotes => [].concat(JSON.parse(newNotes)));
 }
 
-app.get('/api/notes', (req, res) => {
+app.GET('/api/notes', (req, res) => {
     findNotes().then(notes => res.json(notes))
 });
 
@@ -43,4 +56,19 @@ app.POST('/api/notes', (req, res) => {
             alert: 'Note added successfully!'
         }))
     })
-});
+})
+
+app.DELETE('/api/notes:id', (req, res) => {
+    findNotes().then(oldNotes => {
+        console.log(oldNotes)
+        let filteredNotes = oldNotes.filter(notes => notes.id !== req.params.id)
+        writeIt('db/db.json', JSON.stringify(filteredNotes)).then(() => res.json({
+            alert: 'You deleted this note!'
+        }))
+    })
+})
+
+//==============================
+// calls app and listens on PORT location
+//==============================
+app.listen(PORT, () => console.log(`http://${PORT}`));
